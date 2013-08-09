a09f4a7b19f998a3870583ef6f9cfaf083822227
vikeen <john.rake12@gmail.com>
2013-08-09 11:36:56 -0500

    Adding enough code to satisfy feture testing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,29 +2,22 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
   def create
     @post = Post.new(params[:post])
     if @post.save
-      redirect_to @post, flash[:success] => 'Post Successfully created!'
+      redirect_to @post, flash[:success] => 'Post was Successfully created!'
     else
       render 'new'
     end
   end
-  
+
   def new
     @post = Post.new
   end
-  
+
   def edit
     @post = Post.find(params[:id])
-    if @post.update_attributes(params[:post])
-      flash[:success] = 'Post was successfully updated.'
-      redirect_to @post
-    else
-      flash[:error] = 'Failed to update Post.'
-      render action: "edit"
-    end
   end
 
   def show
@@ -32,8 +25,22 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params[:post])
+      flash[:success] = 'Post was successfully updated!'
+      redirect_to @post
+    else
+      flash[:error] = 'Failed to update Post.'
+      render action: :edit
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to posts_url, flash[:success] => "#{@post.title} was successfully deleted."
+    else
+      redirect_to @post, flash[:error] => 'Failed to delete post.'
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  # Returns the full title on a per page basis
+  def full_title(page_title)
+    base_title = 'Web24Studios'
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
+  private
+  # give the ability to find a sessioned user across the application
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
